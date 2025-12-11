796274204f478fdc0a744dd660c5ec8d41003180
ehdgn <ehdgn6570@gmail.com>
2025-12-11 09:48:52 +0900

    mustache create user-entity and dto

--- src/main/java/org/example/demo_ssr_v1/user/User.java
+++ src/main/java/org/example/demo_ssr_v1/user/User.java
@@ -1,24 +1,51 @@
 package org.example.demo_ssr_v1.user;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
 
-@Data
 @NoArgsConstructor
+@Data
 @Table(name = "user_tb")
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String username;
     private String password;
     private String email;
 
     @CreationTimestamp
     private Timestamp createdAt;
+
+    @Builder
+    public User(Timestamp createdAt, String email, Long id, String password, String username) {
+        this.createdAt = createdAt;
+        this.email = email;
+        this.id = id;
+        this.password = password;
+        this.username = username;
+    }
+
+    // 회원 정보 수정 비즈니스 로직 추가
+    // 추후 DTO 설계
+    public void update(UserRequest.UpdateDTO updateDTO) {
+        // 유효성 검사
+        updateDTO.validate();
+
+        this.password = updateDTO.getPassword();
+        this.email = updateDTO.getEmail();
+
+        // 더티 체킹 (변경 감지)
+        // 트랜잭션이 끝나면 자동으로 update 쿼리 진행
+    }
+
 }
